53da1eea9c311bdd8274386a962aa3f59a27191c
Ranjan11022 <ranjankr11022000@gmail.com>
2022-12-17 14:52:58 +0530

    New Todo-Project

---
+++ todonew.js
@@ -0,0 +1,13 @@
+import { cardGen as Card } from './todocard.js';
+
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+const titleInput = document.getElementById('title-input');
+const detailsInput = document.getElementById('details-input');
+const imgInput = document.getElementById('img-input');
+
+btn.addEventListener('click', function () {
+  const card = Card(titleInput.value, detailsInput.value, imgInput.value);
+  container.append(card);
+  
+});
